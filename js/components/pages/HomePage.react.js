d4b3eca177fc8c1ea2cda64ea270d976debb883b
Rafal Szczypka <rszczypka@squaredfinancial.com>
2016-06-14 23:49:26 +0100

    fix: build failing

--- js/components/pages/HomePage.react.js
+++ js/components/pages/HomePage.react.js
@@ -5,15 +5,16 @@
 
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import Select from 'react-select';
+import {Async} from 'react-select';
 import 'react-select/dist/react-select.css';
 import Journeys from '../Journeys.react';
 import {
-    asyncGetStations,
-    asyncGetJourneys,
-    setFromStation,
-    setToStation,
-    setStationError
+  asyncGetFromStations,
+  asyncGetToStations,
+  asyncGetJourneys,
+  setFromStation,
+  setToStation,
+  setStationError
 } from '../../actions/AppActions';
 import _ from 'lodash';
 
@@ -55,76 +56,76 @@ class HomePage extends Component {
 
     render() {
         const dispatch = this.props.dispatch;
-        const {stations, toStationsIsLoading, fromStationsIsLoading, fromStation, toStation, stationError} = this.props.data;
+        const {fromStations, toStations, toStationsIsLoading, fromStationsIsLoading, fromStation, toStation, stationError} = this.props.data;
 
         return (
-            <div>
-                <form className="trainForm" onSubmit={(e) => this.handleSubmit(e)}>
-                    <div className="container">
-                        { stationError &&
-                        <div
-                            role="alert"
-                            className={ (stationError) ? 'alert alert-danger' : '' }
-                        >
-                            <ul className="list-unstyled">
-                                <li>
-                                    <span className="label label-danger">error</span> { stationError }
-                                </li>
-                            </ul>
-                        </div>
-                        }
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <div className="form-group">
-                                    <label htmlFor="departure">From</label>
-                                    <Select.Async
-                                        name="departure"
-                                        id="departure"
-                                        required
-                                        matchProp="label"
-                                        isLoading={ fromStationsIsLoading }
-                                        loadOptions={ (input) => { return dispatch(asyncGetStations(input,'from')).then(() => { return { options: stations }; }) } }
-                                        minimumInput={2}
-                                        searchPromptText="Start typing the name of your FROM station"
-                                        onChange={(selectValue) => this.handleFromChange(selectValue)}
-                                        value={ fromStation }
-                                    />
-                                </div>
-                            </div>
-                            <div className="col-sm-6">
-                                <div className="form-group">
-                                    <label htmlFor="arrival">To</label>
-                                    <Select.Async
-                                        name="arrival"
-                                        id="arrival"
-                                        required
-                                        matchProp="label"
-                                        isLoading={ toStationsIsLoading }
-                                        loadOptions={ (input) => { return dispatch(asyncGetStations(input,'to')).then(() => { return { options: stations }; }) } }
-                                        minimumInput={2}
-                                        searchPromptText="Start typing the name of your TO station"
-                                        onChange={(selectValue) => this.handleToChange(selectValue)}
-                                        value={ toStation }
-                                    />
-                                </div>
-                            </div>
+          <div>
+              <form className="trainForm" onSubmit={(e) => this.handleSubmit(e)}>
+                  <div className="container">
+                      { stationError &&
+                      <div
+                        role="alert"
+                        className={ (stationError) ? 'alert alert-danger' : '' }
+                      >
+                          <ul className="list-unstyled">
+                              <li>
+                                  <span className="label label-danger">error</span> { stationError }
+                              </li>
+                          </ul>
+                      </div>
+                      }
+                      <div className="row">
+                          <div className="col-sm-6">
+                              <div className="form-group">
+                                  <label htmlFor="departure">From</label>
+                                  <Async
+                                    name="departure"
+                                    id="departure"
+                                    required
+                                    matchProp="label"
+                                    isLoading={ fromStationsIsLoading }
+                                    loadOptions={ (input) => { return dispatch(asyncGetFromStations(input)).then(() => { return { options: fromStations }; }) } }
+                                    minimumInput={2}
+                                    searchPromptText="Start typing the name of your FROM station"
+                                    onChange={(selectValue) => this.handleFromChange(selectValue)}
+                                    value={ fromStation }
+                                  />
+                              </div>
+                          </div>
+                          <div className="col-sm-6">
+                              <div className="form-group">
+                                  <label htmlFor="arrival">To</label>
+                                  <Async
+                                    name="arrival"
+                                    id="arrival"
+                                    required
+                                    matchProp="label"
+                                    isLoading={ toStationsIsLoading }
+                                    loadOptions={ (input) => { return dispatch(asyncGetToStations(input)).then(() => { return { options: toStations }; }) } }
+                                    minimumInput={2}
+                                    searchPromptText="Start typing the name of your TO station"
+                                    onChange={(selectValue) => this.handleToChange(selectValue)}
+                                    value={ toStation }
+                                  />
+                              </div>
+                          </div>
 
-                        </div>
-                        <div className="row">
-                            <div className="col-sm-4 col-sm-offset-4 text-center">
-                                <button
-                                    className="btn btn-primary btn-block" type="submit"
-                                    disabled={ (_.isEmpty(fromStation) || _.isEmpty(toStation)) }
-                                >Get The Train
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-                <div className="container">
-                    <Journeys />
-                </div>
-            </div>
+                      </div>
+                      <div className="row">
+                          <div className="col-sm-4 col-sm-offset-4 text-center">
+                              <button
+                                className="btn btn-primary btn-block" type="submit"
+                                disabled={ (_.isEmpty(fromStation) || _.isEmpty(toStation)) }
+                              >Get The Train
+                              </button>
+                          </div>
+                      </div>
+                  </div>
+              </form>
+              <div className="container">
+                  <Journeys />
+              </div>
+          </div>
         );
     }
 }
